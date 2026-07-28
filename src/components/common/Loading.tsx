1f19bccdfd4f4a70417eeb64cd48ec318f13c5dc
1234 <a1234@my-imac.local>
2026-07-28 14:52:22 +0900

    feat: common, ProductCard

---
+++ src/components/common/Loading.tsx
@@ -0,0 +1,10 @@
+import styles from './Loading.module.css';
+function Loading() {
+    return (
+        <div className={styles.loading}>
+            <div className={styles.spinner}></div>
+            <p>로딩 중...</p>
+        </div>
+    );
+}
+export default Loading;
---
+++ src/pages/Cart/Cart.tsx
@@ -0,0 +1,10 @@
+function Cart(){
+    return(
+        <div>
+            <h1>팀원D 장바구니 페이지</h1>
+            <p>송승현이 만들거임</p>
+        </div>
+    )
+}
+
+export default Cart
